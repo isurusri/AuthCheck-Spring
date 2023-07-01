54622bd6aaa486dbf405276a4ae329cb27c72ae4
Isuru Srimal <isuru.srimal258@gmail.com>
2023-07-01 12:29:41 +0530

    Initial commit

---
+++ src/main/java/com/snowl/authcheck/AuthcheckApplication.java
@@ -0,0 +1,13 @@
+package com.snowl.authcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowl/authcheck/AuthcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowl.authcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
